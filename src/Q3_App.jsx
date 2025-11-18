e4f818956adb3640409681723168c31f23891a2d
ryoKato <ryokuun81@gmail.com>
2025-11-18 18:29:19 +0900

    Q3

--- src/Q3_App.jsx
+++ src/Q3_App.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 
 export default function App() {
+// Q2. 次の setState 更新で age は何になる？
+// setUser(prev => {
+//   prev.age = prev.age + 1;
+//   return prev;
+// });
+// **prev.age = prev.age + 1;は正しいがreturn prev;元のオブジェクトと同じ参照
+
 // Q3. 正しく age を 1 増やすコードに直してください。
 // setUser(prev => {
 //   prev.age++;
 //   return prev;
 // });
+// 
 
 const [user, setUser] = useState({name: "kato", age: 20});
 
   const updateAge = () => {
-      setUser( prev=> ({
+      //エラー：setUser をコンポーネント直下で実行しない。これは無限レンダリングになります。更新はイベントなどの中で行うように。
+      setUser( prev=> ({ 
       ...prev, age:prev.age + 1
     }));
   }
